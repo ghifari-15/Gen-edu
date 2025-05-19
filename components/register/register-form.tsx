2271c7ecc1151a6777fce2d9818ac4452015cee6
Gathan Ghifari Rachwiyono <134945443+ghifari-15@users.noreply.github.com>
2025-05-19 15:36:51 +0700

    Remove unused import from RegisterForm component

--- components/register/register-form.tsx
+++ components/register/register-form.tsx
@@ -1,8 +1,6 @@
 "use client"
 
 import type React from "react"
-import { GalleryVerticalEnd } from "lucide-react"
-
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
